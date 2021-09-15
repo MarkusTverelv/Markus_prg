e2101dad6e64361032f13a422b18f5a9638f1225
Markus <martve1214@skola.goteborg.se>
2021-09-15 12:12:48 +0200

    Created a parabolic curve class and constructors

--- Assets/Scripts/Assignment1.cs
+++ Assets/Scripts/Assignment1.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 
 public class Assignment1 : ProcessingLite.GP21
 {
-    float xPos = 4.0f;
-    float yPos = 3.0f;
-
     // Start is called before the first frame update
     void Start()
     {
         Stroke(50, 50, 50);
-        PrintLetterMOutline(xPos, yPos);
-        PrintLetterAOutline(xPos, yPos);
-        PrintLetterROutline(xPos, yPos);
-        PrintLetterKOutline(xPos, yPos);
-        PrintLetterUOutline(xPos, yPos);
-        PrintLetterSOutline(xPos, yPos);
+        PrintLetterMOutline();
+        PrintLetterAOutline();
+        PrintLetterROutline();
+        PrintLetterKOutline();
+        PrintLetterUOutline();
+        PrintLetterSOutline();
 
         Stroke(255, 255, 255);
-        PrintLetterM(xPos, yPos);
-        PrintLetterA(xPos, yPos);
-        PrintLetterR(xPos, yPos);
-        PrintLetterK(xPos, yPos);
-        PrintLetterU(xPos, yPos);
-        PrintLetterS(xPos, yPos);
+        PrintLetterM();
+        PrintLetterA();
+        PrintLetterR();
+        PrintLetterK();
+        PrintLetterU();
+        PrintLetterS();
     }
 
-    private void PrintLetterM(float x, float y)
+    private void PrintLetterM()
     {
         // M
         Line(4, 7, 4, 3);
@@ -36,35 +33,35 @@ public class Assignment1 : ProcessingLite.GP21
         Line(5, 5, 6, 7);
         Line(6, 7, 6, 3);
     }
-    private void PrintLetterA(float x, float y)
+    private void PrintLetterA()
     {
         // A
         Line(8, 3, 9, 7);
         Line(9, 7, 10, 3);
         Line(8.5f, 5, 9.5f, 5);
     }
-    private void PrintLetterR(float x, float y)
+    private void PrintLetterR()
     {
         // R
         Line(12, 3, 12, 6);
         Circle(13, 6, 2);
         Line(12, 5.6f, 14, 3);
     }
-    private void PrintLetterK(float x, float y)
+    private void PrintLetterK()
     {
         // K
         Line(16, 3, 16, 7);
         Line(16, 5, 18, 7);
         Line(16, 5, 18, 3);
     }
-    private void PrintLetterU(float x, float y)
+    private void PrintLetterU()
     {
         // U
         Line(20, 3, 20, 7);
         Line(22, 3, 22, 7);
         Line(20, 3, 22, 3);
     }
-    private void PrintLetterS(float x, float y)
+    private void PrintLetterS()
     {
         //S
         Line(24, 3, 26, 3);
@@ -74,7 +71,7 @@ public class Assignment1 : ProcessingLite.GP21
         Line(24, 7, 26, 7);
     }
 
-    private void PrintLetterMOutline(float x, float y)
+    private void PrintLetterMOutline()
     {
         // M
         Line(4.5f, 7, 4.5f, 3);
@@ -82,35 +79,35 @@ public class Assignment1 : ProcessingLite.GP21
         Line(5.5f, 5, 6.5f, 7);
         Line(6.5f, 7, 6.5f, 3);
     }
-    private void PrintLetterAOutline(float x, float y)
+    private void PrintLetterAOutline()
     {
         // A
         Line(8.5f, 3, 9.5f, 7);
         Line(9.5f, 7, 10.5f, 3);
         Line(8.9f, 5, 10, 5);
     }
-    private void PrintLetterROutline(float x, float y)
+    private void PrintLetterROutline()
     {
         // R
         Line(12.5f, 3, 12.5f, 6);
         Circle(14, 6, 2);
         Line(12.5f, 5.6f, 14.5f, 3);
     }
-    private void PrintLetterKOutline(float x, float y)
+    private void PrintLetterKOutline()
     {
         // K
         Line(16.5f, 3, 16.5f, 7);
         Line(16.5f, 5, 18.5f, 7);
         Line(16.5f, 5, 18.5f, 3);
     }
-    private void PrintLetterUOutline(float x, float y)
+    private void PrintLetterUOutline()
     {
         // U
         Line(20.5f, 3, 20.5f, 7);
         Line(22.5f, 3, 22.5f, 7);
         Line(20.5f, 3, 22.5f, 3);
     }
-    private void PrintLetterSOutline(float x, float y)
+    private void PrintLetterSOutline()
     {
         //S
         Line(25.5f, 3, 26.5f, 3);
--- Assets/Scripts/Assignment2.cs
+++ Assets/Scripts/Assignment2.cs
@@ -6,11 +6,16 @@ public class Assignment2 : ProcessingLite.GP21
 {
     public int lines;
 
+    public int lineDensity;
+    public Vector2 lineStartPoint;
+    public Vector2 lineEndPoint;
+
+    ParabolicCurves pc;
+
     private void Start()
     {
-        for (int x = 0, y = lines; x < lines; x++)
+        for (int x = 0, y = lines; x < lines; x++, y--)
         {
-            y--;
             Line(0, y, x, 0);
 
             if (x % 3 == 0)
@@ -19,15 +24,23 @@ public class Assignment2 : ProcessingLite.GP21
                 Stroke(255);
         }
     }
+    private void Update()
+    {
+        pc = new ParabolicCurves(lineDensity, lineEndPoint, lineStartPoint);
+    }
 }
 public class ParabolicCurves : ProcessingLite.GP21
 {
-    public ParabolicCurves(Vector2 xAxis, Vector2 yAxis, int numberOfLines)
+    public ParabolicCurves(int numberOfLines) : this(numberOfLines, new Vector2(0, 0), new Vector2(0, 0)) { }   //Chain-calling contructors
+    public ParabolicCurves(Vector2 xAxis, Vector2 yAxis) : this(30, xAxis, yAxis) { }   //Chain-calling contructors
+    public ParabolicCurves(int numberOfLines, Vector2 xAxis, Vector2 yAxis)     //Chain-calling contructors
     {
-        for (int i = 0, f = numberOfLines; i < numberOfLines; i++)
-        {
-            f--;
-            Line(yAxis.x, yAxis.y + f, xAxis.x + i, xAxis.y);
-        }
+        CreateCurve(numberOfLines, xAxis, yAxis);
+    }
+
+    private void CreateCurve(int lines, Vector2 axis1, Vector2 axis2)
+    {
+        for (int i = 0, f = lines; i < lines; i++, f--)
+            Line(axis2.x, axis2.y + f, axis1.x + i, axis1.y);
     }
 }
