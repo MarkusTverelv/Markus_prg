ec054f5841c6d144b91e0e761362deb2fb131060
Markus <martve1214@skola.goteborg.se>
2021-09-20 16:00:09 +0200

    finished assignment 4 ish

--- Assets/Scripts/Assignment4.cs
+++ Assets/Scripts/Assignment4.cs
@@ -7,10 +7,10 @@ public class Assignment4 : ProcessingLite.GP21
     float xMovement = 0.0f;
     float yMovement = 0.0f;
 
-    public float velocity = 10.0f;
-    public float speed;
-    public int acceleration;
     public float maxSpeed;
+    public float circleSpeed;
+    public float squareVelocity = 10.0f;
+    public int acceleration;
 
     Vector2 dir;
     Vector2 circlePosition;
@@ -22,7 +22,6 @@ public class Assignment4 : ProcessingLite.GP21
         circlePosition = new Vector2(Width / 2, Height / 2);
         squarePosition = new Vector2(Width / 2 + 5, Height / 2);
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -37,16 +36,44 @@ public class Assignment4 : ProcessingLite.GP21
         dir = new Vector2(xMovement, yMovement);
         dir.Normalize();
 
-        if (xMovement > 0 || yMovement > 0 || xMovement < 0 || yMovement < 0)
-        {
-            circlePosition += dir * Time.deltaTime * speed;
+        Move();
+        ScreenWarp();
+    }
+    private void ScreenWarp()
+    {
+        if (circlePosition.x >= Width)
+            circlePosition.x = 0;
+        else if (circlePosition.x <= 0)
+            circlePosition.x = Width;
 
-            velocity += acceleration * Time.deltaTime;
-            squarePosition += dir * Time.deltaTime * velocity;
+        if (squarePosition.x >= Width)
+            squarePosition.x = 0;
+        else if (squarePosition.x <= 0)
+            squarePosition.x = Width;
+
+        if (circlePosition.y >= Height)
+            circlePosition.y = 0;
+        else if (circlePosition.y <= 0)
+            circlePosition.y = Height;
+
+        if (squarePosition.y >= Height)
+            squarePosition.y = 0;
+        else if (squarePosition.y <= 0)
+            squarePosition.y = Height;
+    }
+    private void Move()
+    {
+        if (xMovement != 0 || yMovement != 0)
+        {
+            circlePosition += dir * Time.deltaTime * circleSpeed;
+
+            squareVelocity += acceleration * Time.deltaTime;
+            squarePosition += dir * squareVelocity * Time.deltaTime;
         }
         else
-        {
-            velocity = 10;
-        }
+            squareVelocity = 10;
+
+        if (squareVelocity > maxSpeed)
+            squareVelocity = maxSpeed;
     }
 }
